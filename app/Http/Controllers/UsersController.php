0ee58a68a1c936bbfa63e13c0bcd2fdf6e4c00d5
nishi-shinya <nishimura-shinya@noah.jp>
2020-10-30 13:21:06 +0900

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        return 'its working';
+    }
+    
+    public function create()
+    {
+        
+    }
+    
+    public function store(Request $request)
+    {
+        
+    }
+}
